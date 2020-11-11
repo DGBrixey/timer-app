fc59ee414f2abaa02bd1b3215d65a69ac35689de
Dan Brixey <danbrixey@gmail.com>
2020-11-11 20:18:09 +0000

    Add method for timer countdown and pause button

--- index.js
+++ index.js
@@ -5,15 +5,26 @@ class Timer {
 		this.pauseButton = pauseButton;
 
 		this.startButton.addEventListener('click', this.start);
+		this.pauseButton.addEventListener('click', this.pause);
 	}
 
-    start() {
-		console.log('Time to start the timer');
-	}
+	start = () => {
+		this.tick();
+		this.interval = setInterval(this.tick, 1000);
+	};
+
+	pause = () => {
+		clearInterval(this.interval);
+	};
+
+	tick = () => {
+		const timeRemaining = parseFloat(this.durationInput.value);
+		this.durationInput.value = timeRemaining - 1;
+	};
 }
 
-const durationInput = document.querySelector('#duration')
-const startButton = document.querySelector('#start')
-const pauseButton = document.querySelector('#pause')
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
 
-const timer =  new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton);
